c44bf204999aaead6429d74e9798c8b9cc5e6cfe
DesCritas <nik1234nik@gmail.com>
2022-09-01 10:33:47 +0300

    HW4 viewBinding added in build.gradle

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,10 +2,15 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<ImageView>(R.id.likesButton).setOnClickListener {
+            println("Clicked likesButton")
+            (it as ImageView).setImageResource(R.drawable.ic_baseline_favorite_24)
+        }
     }
 }
